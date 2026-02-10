4e293eb3779c3399b2df7e4d50f5a8d96a4d6bb7
Saul Hanson <3pathstoperfection@gmail.com>
2026-02-10 10:47:52 -0700

    created a hittable, hittable_list, and sphere class

---
+++ src/general.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.141592653589793285;
+
+// Utility functions
+
+inline double degrees_to_rads(double degrees)
+{
+    return degrees * pi / 180.0;
+}
+
+// common headers
+
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,18 @@
-#include "color.h"
-#include "ray.h"
+#include "general.h"
+
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 #include "vec3.h"
 
-#include <cmath>
-#include <iostream>
 
-double hit_sphere(const point3& center, double radius, const ray& r)
-{
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    
-    if (discriminant < 0)
-    {
-        return -1.0;
-    }
-    else 
-    {
-        return (-b - std::sqrt(discriminant)) / (2.0*a);
-        
-    }
-}
 
-color ray_color(const ray& r)
+color ray_color(const ray& r, const hittable& world)
 {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0)
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec))
     {
-        vec3 normal = normalize(r.at(t) - vec3(0, 0, -1));
-        return 0.5*color(normal.x()+1, normal.y()+1, normal.z()+1);
+        return 0.5 * (rec.normal + color(1,1,1));
     }
     
     vec3 unit_direction = normalize(r.direction());
@@ -45,6 +27,12 @@ int main() {
     int image_width = 400;
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+    
+    // World
+    hittable_list world;
+    
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     auto focal_len = 1.0;
@@ -76,7 +64,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
